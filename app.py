953c66dbedc641c92390b310148cb0edee28060e
David Williams <120070689+WilliamsDave@users.noreply.github.com.>
2024-10-21 19:11:51 -0400

    added filtered results based on aisle

--- app.py
+++ app.py
@@ -34,9 +34,27 @@ def get_top_consequents(rules, top_n=5):
     counts = pd.Series(all_consequents).value_counts()
     return counts.head(top_n).index.tolist()
 
+def find_aisle_recommendations(cart, recommendations, grocery_data):
+    # Check aisles found in user cart
+    aisles = grocery_data[grocery_data['itemDescription'].str.lower().isin(cart)]['itemAisle'].unique()
+    
+    # Get all items found in those aisles
+    aisle_recommendations = grocery_data[grocery_data['itemAisle'].isin(aisles)]
+
+    # Order the new list based on original recommended item list
+    sorted_aisle_recommendations = []
+    for item in recommendations:
+        # If item is in aisle recommendations add to next spot in sorted list
+        if item in aisle_recommendations['itemDescription'].str.lower().tolist():
+            sorted_aisle_recommendations.append(item)
+    
+    # Return top 5 aisle based recommendations
+    return sorted_aisle_recommendations[:5]
+
 # Currently using get and post requests, can change if better method found 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    aisle_recommendations = []
     recommendations = []
     message = ""
     if request.method == 'POST':
@@ -52,17 +70,17 @@ def index():
             message = "Invalid items found"
         else:
             relevant_rules = find_relevant_rules(user_cart, rules)
-
         
             if relevant_rules.empty:
                 message = "No recommendations found"
             else:
                 # Sorts the rules based on lift and gets top "N" recommended items
                 sorted_rules = relevant_rules[['antecedents', 'consequents', 'support', 'confidence', 'lift']].sort_values('lift', ascending=False)
-                recommendations = get_top_consequents(sorted_rules, 100)
-                
+                recommendations = get_top_consequents(sorted_rules)
+            # Filters recommendations based on aisles
+            aisle_recommendations = find_aisle_recommendations(user_cart,  get_top_consequents(sorted_rules, 100), grocery_data)
     # Uses the recommendations found to populate the html
-    return render_template('index.html', recommendations=recommendations, message=message)
+    return render_template('index.html', recommendations=recommendations, aisle_recommendations=aisle_recommendations, message=message)
 
 if __name__ == '__main__':
     app.run(debug=True)
